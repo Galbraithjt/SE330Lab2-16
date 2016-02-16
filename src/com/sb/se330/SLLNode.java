2511a89715b16c7584665525dc367e8794d7f4e4
Joshua <galbraithjt@gmail.com>
2016-02-16 12:27:14 -0800

    Initial Commit

--- src/com/sb/se330/SLLNode.java
+++ src/com/sb/se330/SLLNode.java
@@ -5,16 +5,20 @@ package com.sb.se330;
  */
 
 //*********************** SLLNode.java *******************************
-public class SLLNode {
+public class SLLNode
+{
     public Object info;
     public SLLNode next;
-    public SLLNode() {
+    public SLLNode()
+    {
         next = null;
     }
-    public SLLNode(Object el) {
+    public SLLNode(Object el)
+    {
         info = el; next = null;
     }
-    public SLLNode(Object el, SLLNode ptr) {
+    public SLLNode(Object el, SLLNode ptr)
+    {
         info = el; next = ptr;
     }
 }
