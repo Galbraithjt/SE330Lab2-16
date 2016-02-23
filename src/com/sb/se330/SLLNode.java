168df1d46bf8c3d89064f7bdcabeeff56cfb403c
Joshua <galbraithjt@gmail.com>
2016-02-22 18:28:36 -0800

    Final Commit

--- src/com/sb/se330/SLLNode.java
+++ src/com/sb/se330/SLLNode.java
@@ -4,19 +4,54 @@ package com.sb.se330;
  * Created by Joshua on 2/16/16.
  */
 public class SLLNode
-{
-    public Object info;
+{//start main
+    // Set to public so getters & setters aren't needed
+
+    public String nodeNumber;
+
+    // Reference to next link made in the SLList
+    // Holds the reference to the Link that was created before it
+    // Set to null until it is connected to other links
+
     public SLLNode next;
-    public SLLNode()
+
+    public SLLNode(String nodeNumber)
     {
-        next = null;
+        this.nodeNumber = nodeNumber;
     }
-    public SLLNode(Object el)
+
+    public void display()
     {
-        info = el; next = null;
+        System.out.println(nodeNumber);
     }
-    public SLLNode(Object el, SLLNode ptr)
+
+    public String toString()
     {
-        info = el; next = ptr;
+        return nodeNumber;
+    }
+
+    public static void main(String[] args)
+    {
+        SLList theLinkedList = new SLList();
+
+        // Insert Link and add a reference to the node Link added just prior
+        // to the field next
+
+        theLinkedList.insertFirstLink("1");
+        theLinkedList.insertFirstLink("2");
+        theLinkedList.insertFirstLink("3");
+        theLinkedList.insertFirstLink("4");
+
+        theLinkedList.display();
+
+        System.out.println("Value of first node in Link List " + theLinkedList.firstLink + "\n");
+
+        System.out.println(theLinkedList.find("3").nodeNumber + " Was Found");//finds node 3
+
+        theLinkedList.removeLink("2");//removes node 2
+
+        System.out.println("\n2\n");
+
+        theLinkedList.display();
     }
 }
--- src/com/sb/se330/SLList.java
+++ src/com/sb/se330/SLList.java
@@ -5,54 +5,143 @@ package com.sb.se330;
  */
 public class SLList
 {
-    protected SLLNode head = null;
-    public SLList()
-    {
 
+    // Reference to first Link in list
+    // The last Link added to the LinkList
+
+    public SLLNode firstLink;
+
+    SLList()
+    {
+        // Here to show the first Link always starts as null
+
+        firstLink = null;
     }
+
+    // Returns true if LinkList is empty
+
     public boolean isEmpty()
     {
-        return head == null;
+        return(firstLink == null);
     }
-    public Object first()
+
+    public void insertFirstLink(String nodeNumber)
     {
-        return head.info;
+        SLLNode newLink = new SLLNode(nodeNumber);
+        // Connects the firstLink field to the new Link
+
+        newLink.next = firstLink;
+
+        firstLink = newLink;
     }
-    public void printAll(java.io.PrintStream out)
+
+    public void display()
     {
-        for (SLLNode tmp = head; tmp != null; tmp = tmp.next)
-            out.print(tmp.info);
+        SLLNode theLink = firstLink;
+
+        // Start at the reference stored in firstLink and
+        // keep getting the references stored in next for
+        // every Link until next returns null
+
+        while(theLink != null)
+        {
+            theLink.display();
+
+            System.out.println("Next Link: " + theLink.next);
+
+            theLink = theLink.next;
+
+            System.out.println();
+        }
     }
-    public void add(Object el)
+
+    public SLLNode find(String nodeNumber)
     {
-        head = new SLLNode(el,head);
+        SLLNode theLink = firstLink;
+
+        if(!isEmpty())
+        {
+            while(theLink.nodeNumber != nodeNumber)
+            {
+                // Checks if at the end of the LinkList
+
+                if(theLink.next == null)
+                {
+                    // Got to the end of the Links in LinkList
+                    // without finding a match
+
+                    return null;
+                }
+
+                else
+                {
+                    // Found a matching Link in the LinkList
+
+                    theLink = theLink.next;
+                }
+            }
+        }
+
+        else
+        {
+            System.out.println("Empty LinkList");
+        }
+
+        return theLink;
     }
-    public Object find(Object el)
+
+    public SLLNode removeLink(String nodeNumber)
     {
-        SLLNode tmp = head;
-        for ( ; tmp != null && !el.equals(tmp.info); tmp = tmp.next);
-        if (tmp == null)
-            return null;
-        else return tmp.info;
-    }
-    public Object deleteHead()
-    { // remove the head and return its info;
-        Object el = head.info;
-        head = head.next;
-        return el;
-    }
-    public void delete(Object el)
-    { // find and remove el;
-        if (head != null) // if nonempty list;
-            if (el.equals(head.info)) // if head needs to be removed;
-                head = head.next;
+
+        SLLNode currentLink = firstLink;
+        SLLNode previousLink = firstLink;
+
+        // Keep searching as long as a match isn't made
+
+        while(currentLink.nodeNumber != nodeNumber)
+        {
+            // Check if at the last Link in the LinkList
+
+            if(currentLink.next == null)
+            {
+                // nodeNumber not found so leave the method
+
+                return null;
+            }
+
             else
             {
-                SLLNode pred = head, tmp = head.next;
-                for ( ; tmp != null && !(tmp.info.equals(el));
-                      pred = pred.next, tmp = tmp.next);
-                if (tmp != null) // if found
-                    pred.next = tmp.next;
+                // We checked here so let's look in the
+                // next Link on the list
+
+                previousLink = currentLink;
+
+                currentLink = currentLink.next;
             }
+        }
+
+        if(currentLink == firstLink)
+        {
+            // If you are here that means there was a match
+            // in the reference stored in firstLink in the
+            // LinkList so just assign next to firstLink
+
+            firstLink = firstLink.next;
+        }
+
+        else
+        {
+            // If you are here there was a match in a Link other
+            // than the firstLink. Assign the value of next for
+            // the Link you want to delete to the Link that's
+            // next previously pointed to the reference to remove
+
+            System.out.println("FOUND A MATCH");
+            System.out.println("currentLink: " + currentLink);
+            System.out.println("firstLink: " + firstLink);
+
+            previousLink.next = currentLink.next;
+        }
+        return currentLink;
     }
 }
